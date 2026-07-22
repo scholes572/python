e507feacf36eaa7bba1aa9d5d34cac149dcb5b4e
Agostino <agostinoscholes572@gmail.com>
2026-07-22 18:00:20 +0800

    Still practising while loops

--- while_loops.py
+++ while_loops.py
@@ -1,6 +1,15 @@
 #While loops - A statement that will execute its block of code, as long as its condition remains true
-name = None
+count = 1
+while count <= 3:
+    print(f"Loop running: {count}")
+    count += 1
+print("Loop finished")
 
-while not name:
-    name = input("Enter your name: ")
-print("Hello " + name)
+total = 0 #Starts with a 0 because nothing is in the sum yet.
+user_input = -1
+
+while user_input != 0:
+    user_input = int(input("Enter a number to add then if you want the total, Press 0 to stop: "))
+    total += user_input
+print(f"The total sum is : {total}")
+
